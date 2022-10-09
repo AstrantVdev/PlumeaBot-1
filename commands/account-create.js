69ca0ac563886d5cd88ce0701af7d64ab96dd9ab
AstrantV <AstrantV.creator@gmail.com>
2022-10-09 22:23:23 +0200

    Add files via upload

--- commands/account-create.js
+++ commands/account-create.js
@@ -12,24 +12,8 @@ module.exports = {
 
 	async execute(interaction) {
         const user = interaction.options.getUser('user');
-        const editJsonFile = require("edit-json-file")
-
-        const json = require("../utils/json.js")
-        const id = json.intToABC(user.id)
-        const membersfile = editJsonFile(CHANNELSPATH);
-
-        members = membersfile.get("members.list")
-        const today = new Date()
-        const date = ("0" + today.getDate()).slice(-2);
-        const month = ("0" + (today.getMonth() + 1)).slice(-2);
-        const year = today.getFullYear();
-        members.push(id)
-
-        await membersfile.set("members.list", members)
-        await membersfile.set("members." + id +".date", year+month+date)
-        await membersfile.set("members." + id+".plumes", 0)
-        await membersfile.set("members." + id+".scriptucoins", 0)
-        await membersfile.save()
+        const account = require("../utils/account")
+        account.create(user)
 
         interaction.reply({ephemeral:true,content:"https://tenor.com/view/mujikcboro-seriymujik-gif-24361533"});
 
