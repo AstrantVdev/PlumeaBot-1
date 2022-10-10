d04f8da89edaec2acaa312587d77e938b091b8b3
AstrantV <AstrantV.creator@gmail.com>
2022-10-10 20:30:46 +0200

    Update messageCreate.js

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -3,7 +3,6 @@ module.exports = {
 	async execute(message) {
 
         if (!message.author.bot){
-		message.reply("https://tenor.com/view/rimworld-thrumbo-gif-22303388\nLibérez les paëllas");
 		
             let roles = message.member.roles.cache.map(r => `${r}`).length;
 
