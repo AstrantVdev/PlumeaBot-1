395b9bf28fdd0de887b2b1fae1b1050664e930fe
AstrantV <AstrantV.creator@gmail.com>
2022-10-15 16:10:38 +0200

    Add files via upload

--- commands/plumes.js
+++ commands/plumes.js
@@ -26,7 +26,7 @@ module.exports = {
             const plume = require("../utils/plume.js")
             const json = require("../utils/json.js")
             const leaderboard = require("../utils/leaderboard.js")
-            const data = require("../utils/data.js")
+            const dataUtil = require("../utils/data.js")
             const editJsonFile = require("edit-json-file")
 
             const member = interaction.options.getMember('user')
@@ -34,22 +34,24 @@ module.exports = {
             const memberId = member.id
             const stringId = json.intToABC(memberId)
             const p = interaction.options.getInteger('plumes')    
+            const data = editJsonFile(DATA)
 
-            let jsonfile = editJsonFile(DATA);
-            plumes = jsonfile.get("members."+stringId + ".plumes")
+            await (plumes = parseInt(data.get("members."+stringId + ".plumes")), 
+            plumes +=  p)
 
-            await jsonfile.set("members." + stringId + ".plumes", plumes+p)
-            await jsonfile.save()
+            await data.set("members." + stringId + ".plumes", plumes)
+            await data.save()
 
-            await plume.roles(member, plumes+p, interaction)
+            await( message = "**<@" + memberId + "> possède maintenant *" + plumes + "* plumes <:Scriptuplume:1027094890099781673>**\n",
+            message += p+" plumes\n",
+            message += dt+"\n") //dont MOVE ! or plumes will be equel to 0 bruh
+
+            await plume.roles(member, plumes, interaction)
 
             await leaderboard.edit()
 
-            await data.upload()
+            await dataUtil.upload()
 
-            message = "**<@" + memberId + "> possède maintenant *" + plumes+p + "* plumes <:Scriptuplume:1027094890099781673>**\n"
-            message += p+" plumes\n"
-            message += dt+"\n"
             await interaction.reply(message)
                 
         }
