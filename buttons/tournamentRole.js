3f41afa70eb67c38e95bc5f6f2e8738d0f2c297b
AstrantV <AstrantV.creator@gmail.com>
2022-12-26 15:14:34 +0100

    Update tournamentRole.js

--- buttons/tournamentRole.js
+++ buttons/tournamentRole.js
@@ -1,42 +1,27 @@
-const { SlashCommandBuilder, PermissionFlagsBits } = require("discord.js");
 const editJsonFile = require("edit-json-file")
-const dataConfig = editJsonFile(DATA_CONFIG)
+const dadataConfig = editJsonFile(DATA_CONFIG)
 
 module.exports = {
-	data: new SlashCommandBuilder()
-	.setName("button")
-    .setDescription("Create a button")
-    .setDefaultMemberPermissions(PermissionFlagsBits.Administrator)
-	.addStringOption(option =>
-		option.setName("name")
-			.setDescription("Button's name")
-			.addChoices(
-				{ name: "tournamentRole", value: "1" }
-			)), 
+    
+    async execute(member, interaction){
+        tournamentRole = dataConfig.get("rolesId.tournament")
 
-	async execute(interaction) {
-        const value = interaction.options.getString("name")
+        if(member.roles.cache.has(tournamentRole)){
+            await member.roles.remove(tournamentRole)
+            await interaction.reply({content:"Okay... ;-;",ephemeral:true})
 
-        switch(value){
-            case "1":
-                await interaction.channel.send({content : `Appuis sur ce bouton pour avoir le rôle ${dataConfig.get("roleId.tournament")} :`
-                , components: await this.tournamentRole()})
+        }else{
+            await member.roles.add(tournamentRole)
+            await interaction.reply({content:
+
+                `Vous avez désormais accès aux salons 
+                <@#${dadataConfig.get("channels.tournamentRules")}> 
+                et <@#${dadataConfig.get("channels.monthlyNovel")}>`
+
+                ,ephemeral:true})
+
         }
-        
-		await interaction.reply({ content: "Action accomplie avec succès ! :D", ephemeral: true })
 
-	},
-
-    tournamentRole(){
-        const sprintRole = new ActionRowBuilder()
-        .addComponents(
-            new ButtonBuilder()
-                .setCustomId("tournamentRole")
-                .setEmoji("1056933963198890045>")
-                .setStyle("SUCCESS")
-        )
-
-        return sprintRole
-    },
+    }
 
 }
