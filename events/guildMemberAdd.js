b80b8c4c461d2c602c6ae842fcb948280c804fe2
AstrantV <AstrantV.creator@gmail.com>
2022-12-26 20:41:12 +0100

    Update guildMemberAdd.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,25 +1,31 @@
-const { EmbedBuilder } = require('discord.js');
-
 module.exports = {
-	name: 'guildMemberRemove',
+	name: 'guildMemberAdd',
 	once: false,
 	async execute(member) {
         const editJsonFile = require("edit-json-file")
+        const message = require("../utils/message")
 
         const channelfile = editJsonFile(DATA_CONFIG);
-        const welcome = channelfile.get("channels.cya")
+        const welcome = channelfile.get("channels.welcome")
+        const presentation = channelfile.get("presentation")
+        const user = member.user
 
-        const welcomeMessage = new EmbedBuilder()
-        .setColor(0x2C2F33)
-        .setDescription(`**${member.user} nous a quitté !!!**`)
-        .setAuthor({ name: 'Niooon !',iconURL: 'https://i.imgur.com/TYeapMy.png', url: 'https://www.youtube.com/watch?v=xvFZjo5PgG0' })
+        welcomeMessage = message.newEmbed()
+        .setDescription(`**Bienvenue sur Scriptura ${member.user}.**`)
+        .setAuthor({ name: 'Youpiii !',iconURL: 'https://i.imgur.com/TYeapMy.png', url: 'https://tenor.com/view/rickroll-roll-rick-never-gonna-give-you-up-never-gonna-gif-22954713' })
         .setThumbnail(member.user.displayAvatarURL())
-        .setTimestamp()
+        
+        const json = require("../utils/json.js")
+        const id = json.intToABC(member.user.id)
+        const data = editJsonFile(DATA);
 
-        const dataUtils = require("../utils/data.js")
-        await dataUtils.accountRemove(member.user)
+        members = data.get("members.list")
+        if(!members.includes(id)&& !user.bot){
+                const dataUtil = require("../utils/data")
+                dataUtil.accountCreate(member.user)
+        }
+
         await client.channels.cache.fetch(welcome)
-        .then(channel => channel.send({ embeds: [welcomeMessage]}));
-		
-	},
-};
+        .then(channel => channel.send({ embeds: [welcomeMessage]}))
+	}
+}
