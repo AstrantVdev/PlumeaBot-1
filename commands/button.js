76613e028efcc2e6ec9bba28be6d655eda3a9a89
AstrantV <AstrantV.creator@gmail.com>
2022-12-27 16:08:44 +0100

    Create button.js

---
+++ commands/button.js
@@ -0,0 +1,43 @@
+const { SlashCommandBuilder, PermissionFlagsBits } = require("discord.js");
+const { ActionRowBuilder, ButtonBuilder, ButtonStyle} = require('discord.js')
+const editJsonFile = require("edit-json-file")
+const dataConfig = editJsonFile(DATA_CONFIG)
+
+module.exports = {
+	data: new SlashCommandBuilder()
+	.setName("button")
+    .setDescription("Create a button")
+    .setDefaultMemberPermissions(PermissionFlagsBits.Administrator)
+	.addStringOption(option =>
+		option.setName("name")
+			.setDescription("Button's name")
+			.addChoices(
+				{ name: "tournamentRole", value: "1" })
+            .setRequired(true)), 
+
+	async execute(interaction) {
+        const value = interaction.options.getString("name")
+
+        switch(value){
+            case "1":
+                await interaction.channel.send({content : `Appuyez sur le bouton pour participer au <@&${await dataConfig.get("rolesId.tournament")}> :`
+                , components: [await this.tournamentRole()]})
+        }
+        
+		await interaction.reply({ content: "Action accomplie avec succès ! :D", ephemeral: true })
+
+	},
+
+    tournamentRole(){
+        const sprintRole = new ActionRowBuilder()
+        .addComponents(
+            new ButtonBuilder()
+                .setCustomId("tournamentRole")
+                .setEmoji("<:Scriptuplume:1027094890099781673>")
+                .setStyle(ButtonStyle.Danger)
+        )
+
+        return sprintRole
+    },
+
+}
