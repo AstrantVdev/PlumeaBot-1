33e938d717a4653c2601cf03878b2b26eaf69cca
AstrantV <AstrantV.creator@gmail.com>
2022-12-28 16:37:58 +0100

    Create cleandata.js

---
+++ commands/cleandata.js
@@ -0,0 +1,33 @@
+const { SlashCommandBuilder, PermissionFlagsBits } = require("discord.js");
+const editJsonFile = require("edit-json-file")
+const data = editJsonFile(DATA)
+
+module.exports = {
+	data: new SlashCommandBuilder()
+	.setName("cleandata")
+    .setDefaultMemberPermissions(PermissionFlagsBits.Administrator), 
+
+	async execute(interaction) {
+        const members = data.get("members.list")
+        const correctMembers = []
+
+        await members.forEach(m => {
+            
+            try {
+                const date = data.get("members."+m+".date")
+                correctMembers.add(m)
+                
+            } catch (error) {
+                
+            }
+            
+        })
+
+        await data.set("members.list", correctMembers)
+        await data.save()
+
+        interaction.reply({content:"ez",ephemeral:true})
+
+	}
+
+}
