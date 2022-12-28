b2b23430386f90bd2a172e5b73f27509be989fdf
AstrantV <AstrantV.creator@gmail.com>
2022-12-28 16:45:40 +0100

    Update deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -2,10 +2,12 @@ const fs = require('node:fs');
 const path = require('node:path');
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord.js');
-global.DATA_CONFIG = "DATACONFIG.json"
 const commands = [];
 const commandsPath = path.join(__dirname, 'commands');
 const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+global.DATA_CONFIG = "DATA_CONFIG.json"
+global.DATA = "DATA.json"
 
 for (const file of commandFiles) {
 	const filePath = path.join(commandsPath, file);
