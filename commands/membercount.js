32c1c8ac2d20663dbb89179e668f6ce71fdd5c63
AstrantV <AstrantV.creator@gmail.com>
2023-01-19 13:21:32 +0100

    Update membercount.js

--- commands/membercount.js
+++ commands/membercount.js
@@ -8,35 +8,32 @@ module.exports = {
     async execute(interaction) {
         let count = interaction.guild.memberCount
         count -= interaction.guild.members.cache.filter(m => m.user.bot).size
+        await interaction.reply("> 📕▸**Bienvenue sur Pluméa !** 🧭 ▸Le guide du Pluméen contient toutes les informations nécessaires sur le bon fonctionnement de la communauté : *règles générales, comment poster son avis, comment poster son texte...*\n"+
+"https://discord.com/channels/1027089727360344144/1063760987238436924\n\n"+
 
-        await interaction.reply("> 📕▸**Bienvenue sur Pluméa !**
+"👤 ▸Et si vous nous en disiez plus sur vous ? \n"+
+"https://discord.com/channels/1027089727360344144/1060681609751310336\n\n"+
 
-🧭 ▸Le guide du Pluméen contient toutes les informations nécéssaires sur le bon fonctionnement de la communauté : *règles générales, comment poster son avis, comment poster son texte...*
-https://discord.com/channels/1027089727360344144/1063760987238436924
+"📖▸Postez votre texte et retrouvez ceux des membres !\n"+
+"https://discord.com/channels/1027089727360344144/1060687578413674646\n\n"+
 
-👤 ▸Et si vous nous en disiez plus sur vous ? 
-https://discord.com/channels/1027089727360344144/1060681609751310336
+"📋▸Retrouvez un espace pour poster vos commentaires !\n"+
+"https://discord.com/channels/1027089727360344144/1060687321579659425\n\n"+
 
-📖▸Postez votre texte et retrouvez ceux des membres !
-https://discord.com/channels/1027089727360344144/1060687578413674646
+"💬 ▸Passez nous dire bonjour, ne soyez pas timide !\n"+
+"https://discord.com/channels/1027089727360344144/1060677819107115088\n\n"+
 
-📋▸Retrouvez un espace pour poster vos commentaires !
-https://discord.com/channels/1027089727360344144/1060687321579659425
+"📬▸Un tournois de nouvelle est organisée tous les mois. A vos plumes !\n"+
+"https://discord.com/channels/1027089727360344144/1060679258579669083\n\n"+
 
-💬 ▸Passez nous dire bonjour, ne soyez pas timide !
-https://discord.com/channels/1027089727360344144/1060677819107115088
+"🎫▸Un soucis ? Une question ? L'équipe de Pluméa veille.\n"+
+"https://discord.com/channels/1027089727360344144/1060681924877766786\n\n"+
 
-📬▸Un tournois de nouvelle est organisée tous les mois. A vos plumes !
-https://discord.com/channels/1027089727360344144/1060679258579669083
+"> 📸 nos réseaux :\n"+
+"__INSTAGRAM__\n"+
+"https://instagram.com/plumea.fr?igshid=ZDdkNTZiNTM=\n\n"+
 
-🎫▸Un soucis ? Une question ? L'équipe de Pluméa veille. 
-https://discord.com/channels/1027089727360344144/1060681924877766786
-
-> 📸 nos réseaux :
-__INSTAGRAM__
-https://instagram.com/plumea.fr?igshid=ZDdkNTZiNTM=
-
-https://imgur.com/92562no")
+"https://imgur.com/92562no")
     const o = "**Aujourd'hui Scriptura compte ||   " + count  + "   || âmes ! :D**"
     }
 }
