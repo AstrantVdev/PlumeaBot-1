238aa76165b39d8a7d168a92479db95a778daaf7
AstrantV <AstrantV.creator@gmail.com>
2023-02-02 20:52:19 +0100

    Update sesame.js

--- commands/sesame.js
+++ commands/sesame.js
@@ -1,1 +1,20 @@
+const { SlashCommandBuilder } = require('discord.js');
 
+module.exports = {
+	data: new SlashCommandBuilder()
+	.setName('sesame')
+    .setDescription("Permet d'accéder au serveur uwu")
+    .addStringOption(option => option
+        .setName('pass')
+		.setDescription("Mot de passe dans la description du salon")
+        .setRequired(true)),
+
+	async execute(interaction) {
+        const n = interaction.options.getString('pass')
+        interaction.reply({ content: 'Bienvenue uwu', ephemeral: true })
+    
+    
+
+	}
+
+}
