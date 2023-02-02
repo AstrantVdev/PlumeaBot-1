886954638cbfe5de42398964f208e83e82d51034
AstrantV <AstrantV.creator@gmail.com>
2023-02-02 20:53:45 +0100

    Update sesame.js

--- commands/sesame.js
+++ commands/sesame.js
@@ -12,6 +12,18 @@ module.exports = {
 	async execute(interaction) {
         const n = interaction.options.getString('pass')
         interaction.reply({ content: 'Bienvenue uwu', ephemeral: true })
+		
+		        sprinterRole = dataConfig.get("rolesId.sprinter")
+
+        if(member.roles.cache.has(sprinterRole)){
+            await member.roles.remove(sprinterRole)
+            await interaction.reply({content:"Tu n'es plus un sprinter à présent ! :3\nTu seras donc plus jamais mentionné ! Tous du moins pour les sprints...",ephemeral:true})
+
+        }else{
+            await member.roles.add(sprinterRole)
+            await interaction.reply({content:"Tu es sprinter à présent ! :3\nTu seras donc mentionné à chaque sprint...",ephemeral:true})
+
+        }
     
     
 
