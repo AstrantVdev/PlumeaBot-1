42883e31c8afce5b5484098911c9be669f823b18
Parabellum1536 <parabellum1536@gmail.com>
2023-02-15 21:52:18 +0100

    credits
    noueavuté

--- commands/profil.js
+++ commands/profil.js
@@ -4,10 +4,10 @@ const { EmbedBuilder } = require('discord.js');
 module.exports = {
 	data: new SlashCommandBuilder()
 	.setName('profil')
-    .setDescription("Profil d'un scripturien")
+    .setDescription("Profil d'un Pluméen")
 	.addUserOption(option => option
         .setName('user')
-		.setDescription("Scripturien")
+		.setDescription("Pluméen")
         .setRequired(true)),
 	async execute(interaction) {
 		const json = require("../utils/json")
@@ -18,12 +18,12 @@ module.exports = {
 		const id = json.intToABC(user.id)
 		const plumes = data.get("members."+id+".plumes")
 		const date = data.get("members."+id+".date")
-		const scriptucoins = data.get("members."+id+".scriptucoins")
+		const credits = data.get("members."+id+".scriptucoins")
 
 		message = "**Profil de : <@"+user.id+">**\n\n"
 		message += "Plumes : *"+plumes+"*\n"
 		message += "Arrivée : *"+date+"*\n"
-		message += "Scriptucoins : *"+scriptucoins+"*\n"
+		message += "Crédits Pluméens : *"+credits+"*\n"
 
 		const messageUtil = require('../utils/message');
         const messageEmbed = messageUtil.newEmbed()
